93d7fc5989a52f6620aff6e666fa6b3e8b4f4f6f
Fabjan Lashi <kaligrafik@gmail.com>
2015-04-11 23:45:01 +0200

    deleted some debug files locally
    just ignore this commit

--- _protected/config/db.php
+++ _protected/config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=k2',
     'username' => 'root',
-    'password' => '',
+    'password' => 'root',
     'charset' => 'utf8',
 ];
